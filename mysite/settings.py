fd27ac2ff8cc2649ee4166ee61a4f23578d5f0eb
Claire Miller <clairemiller@outlook.com>
2022-07-18 14:29:59 -0700

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,6 +105,9 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
 #DATABASES = {
 #        'default': {
 #            'ENGINE': 'django.db.backends.postgresql_psycopg2',

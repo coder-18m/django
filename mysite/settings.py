1166aad4622e70b3d6c4969d7a4e7cc9a2376ca7
Claire Miller <clairemiller@outlook.com>
2022-07-20 14:57:31 -0700

    added adam email

--- mysite/settings.py
+++ mysite/settings.py
@@ -182,7 +182,7 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "contactcalv@gmail.com"
+EMAIL_HOST_USER = "adam.marshall4747@gmail.com"
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 #EMAIL_HOST_PASSWORD = "nmhyhpphjsoceibo"
 
